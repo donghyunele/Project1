7c852ea9b9248d1bb2a55e3adba5dba14b95c1db
donghyun <donghyunele@gmail.com>
2018-08-11 12:33:39 +0900

    페이징 처리
    JPA PageRequest 및 Pageable 사용해서 페이지 처리
    
    List에 담기는 값은 총 몇페이지 중에 몇번째 데이터인지를 리턴해준다.

--- src/main/java/com/example/demo/dao/BoardDAO.java
+++ src/main/java/com/example/demo/dao/BoardDAO.java
@@ -1,12 +1,11 @@
 package com.example.demo.dao;
 
 import com.example.demo.entity.Board;
-import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-import java.util.List;
-
-public interface BoardDAO extends JpaRepository<Board, Integer>, JpaSpecificationExecutor<Board> {
-    List<Board> findAll(Specification<Board> isDeleted);
+public interface BoardDAO extends JpaRepository<Board, Integer>, JpaSpecificationExecutor<Board>{
+    Page<Board> findAll(Pageable pageable);
 }
--- src/main/java/com/example/demo/entity/Board.java
+++ src/main/java/com/example/demo/entity/Board.java
@@ -4,10 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.sql.Date;
 
 @Entity
@@ -17,7 +14,7 @@ import java.sql.Date;
 public class Board {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "board_num")
     private int boardNum;
 
