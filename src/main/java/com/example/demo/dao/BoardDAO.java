1040db2f5528b8fb0e5f156267b611e77e06e446
donghyun <donghyunele@gmail.com>
2018-08-11 12:37:17 +0900

    add Specification

--- src/main/java/com/example/demo/dao/BoardDAO.java
+++ src/main/java/com/example/demo/dao/BoardDAO.java
@@ -3,9 +3,10 @@ package com.example.demo.dao;
 import com.example.demo.entity.Board;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 public interface BoardDAO extends JpaRepository<Board, Integer>, JpaSpecificationExecutor<Board>{
-    Page<Board> findAll(Pageable pageable);
+    Page<Board> findAll(Specification<Board> isDeleted, Pageable pageable);
 }
